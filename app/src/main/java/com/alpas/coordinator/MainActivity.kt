c433c6b1a189cff7b1c189234b50320dc77883bd
Alexey <alexey@Oleksiys-MBP.fritz.box>
2021-11-22 22:56:44 +0100

    implemented layout

--- app/src/main/java/com/alpas/coordinator/MainActivity.kt
+++ app/src/main/java/com/alpas/coordinator/MainActivity.kt
@@ -2,10 +2,39 @@ package com.alpas.coordinator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Checkable
+import com.alpas.coordinator.databinding.ActivityMainBinding
+import com.alpas.coordinator.databinding.LayoutBottombarBinding
+import com.alpas.coordinator.databinding.LayoutSubmenuBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private lateinit var btmBarBinding: LayoutBottombarBinding
+    private lateinit var subMenuBinding: LayoutSubmenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        btmBarBinding = LayoutBottombarBinding.bind(binding.root)
+        subMenuBinding = LayoutSubmenuBinding.bind(binding.root)
+        setContentView(binding.root)
+        setupBottombar()
+    }
+
+    private fun setupBottombar() {
+        btmBarBinding.btnLike.setOnClickListener {
+            it as Checkable
+            it.toggle()
+            Snackbar.make(it, if (it.isChecked) "set like" else "unset like" , Snackbar.LENGTH_LONG)
+                .setAnchorView(binding.bottombar)
+                .show()
+        }
+
+        btmBarBinding.btnSettings.setOnClickListener {
+            it as Checkable
+            it.toggle()
+            if (it.isChecked) binding.submenu.open() else binding.submenu.close()
+        }
     }
 }
